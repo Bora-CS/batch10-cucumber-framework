0c2dc281c4e3a7d9ca4bfea9c8a9c9697bf89c9d
Karma Paljor <kg@karmas-MBP.fios-router.home>
2023-07-30 16:02:17 -0400

    updated

---
+++ src/test/java/selenium/BoraLogin.java
@@ -0,0 +1,21 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BoraLogin {
+
+	public static void main(String[] args) {
+	
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("hhttps://boratech-practice-app.onrender.com/");
+	
+ 
+		
+		
+		
+	}
+
+}
