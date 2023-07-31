3c0eab46a2d8c1f99eff9f4cd86627a90c18ed17
Hui Yang <139652992+HuiYang37@users.noreply.github.com>
2023-07-31 12:06:09 -0400

    Update BoraTechLogin.java

--- src/test/java/selenium/BoraTechLogin.java
+++ src/test/java/selenium/BoraTechLogin.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
- package selenium;
-=======
 package selenium;
->>>>>>> master
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,17 +11,6 @@ public class BoraTechLogin {
 		WebDriver driver = new ChromeDriver();
 
 		try {
-<<<<<<< HEAD
-			driver.get("http://boratech-practice-app.onrender.com/");
-			driver.findElement(By.linkText("Login")).click();
-
-			driver.findElement(By.xpath("//input[@name='email']")).sendKeys("testemail@gmail.com");
-			driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Sewon12345");
-
-			driver.findElement(By.xpath("//input[@type='submit']")).click();
-
-			Thread.sleep(3000);
-=======
 			driver.navigate().to("https://boratech-practice-app.onrender.com/");
 			driver.findElement(By.linkText("Login")).click();
 
@@ -35,7 +20,6 @@ public class BoraTechLogin {
 			driver.findElement(By.xpath("//input[@type='submit']")).click();
 
 			Thread.sleep(2000);
->>>>>>> master
 
 			// Validation
 			String titleText = driver.findElement(By.xpath("//h1[@class='large text-primary']")).getText();
@@ -52,10 +36,6 @@ public class BoraTechLogin {
 			driver.findElement(By.xpath("//a[@href='#!']/span[text()='Logout']"));
 
 			System.out.println("Test Passed");
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 		} catch (Exception e) {
 			System.out.println("Test Failed");
 			System.out.println("Reason: " + e.getMessage());
@@ -64,8 +44,5 @@ public class BoraTechLogin {
 		}
 
 	}
-<<<<<<< HEAD
-=======
 
->>>>>>> master
 }
