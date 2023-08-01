0fcdaec273b89ce4047a026bc33de62aa17a9b17
Karma Paljor <kg@karmas-MacBook-Pro.local>
2023-08-01 15:12:45 -0400

    updated

--- src/test/java/karma/KarmaAddEducationTest.java
+++ src/test/java/karma/KarmaAddEducationTest.java
@@ -1,4 +1,4 @@
-package karmap;
+package karma;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,13 +14,21 @@ public class KarmaAddEducationTest {
 
 			driver.get("https://boratech-practice-app.onrender.com/");
 			driver.findElement(By.linkText("Login")).click();
-			Thread.sleep(5000);
+
 			driver.findElement(By.xpath("//input[@name='email']")).sendKeys("kpaljor306@gmail.com");
 			driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Bora123");
-
-			Thread.sleep(5000);
 			driver.findElement(By.xpath("//a[@href='/add-education']")).click();
-			driver.findElement(By.xpath("//a[@href='#!']/span"));
+			driver.findElement(By.xpath("//input[@name='school']")).sendKeys("Bora Tech");
+			driver.findElement(By.xpath("//input[@name='degree']")).sendKeys("Automation Engineer");
+			driver.findElement(By.xpath("//input[@name='fieldofstudy']")).sendKeys("Automation Engineering");
+			driver.findElement(By.xpath("//input[@name='from']")).sendKeys("06/08/2021");
+			driver.findElement(By.xpath("//input[@name='to']")).sendKeys("07/09/2023");
+			driver.findElement(By.xpath("//textarea[@name='description']")).sendKeys("Test Automation Engineer, "
+					+ "you will play a vital role in ensuring the smooth functioning and reliability of software "
+					+ "applications through automated testing processes");
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			driver.findElement(By.xpath("//a[@href='/']")).click();
+			driver.findElement(By.xpath("//a[@href='#!']/span")).click();
 
 			System.out.println("Pass");
 
--- src/test/java/karma/KarmaAddExperienceTest.java
+++ src/test/java/karma/KarmaAddExperienceTest.java
@@ -1,5 +1,5 @@
 
-package karmap;
+package karma;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -18,9 +18,20 @@ public class KarmaAddExperienceTest {
 		
 			driver.findElement(By.xpath("//input[@name='email']")).sendKeys("kpaljor306@gmail.com");
 			driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Bora123");
-
 			driver.findElement(By.xpath("//a[@href='/add-experience']")).click();
-			driver.findElement(By.xpath("//a[@href='#!']/span"));
+			driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Automation Engineer");
+			driver.findElement(By.xpath("//input[@name='company']")).sendKeys("Bora Tech");
+			driver.findElement(By.xpath("//input[@name='location']")).sendKeys("Virginia");
+			driver.findElement(By.xpath("//input[@name='from']")).sendKeys("06/06/2019");
+			driver.findElement(By.xpath("//input[@name='to']")).sendKeys("07/30/2023");
+			driver.findElement(By.xpath("//textarea[@name='description']")).sendKeys("Design, "
+					+ "develop, and maintain automated test scripts using industry-standard "
+					+ "testing tools and frameworks");
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			driver.findElement(By.xpath("//a[@href='/']")).click();
+			
+			
+			driver.findElement(By.xpath("//a[@href='#!']/span")).click();
 
 			System.out.println("Pass");
 
