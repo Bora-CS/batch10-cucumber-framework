c5bceee3dd6d9a06c3b1a0d701ca23eec11e03b8
Hui Yang <hui.yang.3737@gmail.com>
2023-08-04 10:24:19 -0400

    updated ApplicationFormTest

--- src/test/java/hui_automation/ApplicationFormTest.java
+++ src/test/java/hui_automation/ApplicationFormTest.java
@@ -3,6 +3,7 @@ package hui_automation;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,6 +22,7 @@ public class ApplicationFormTest {
 
 		try {
 			testDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+			testDriver.manage().window().maximize();
 			testDriver.get("https://boratech-practice-app.onrender.com/apply");
 			WebElement sumbitButton = testDriver.findElement(By.xpath("//input[@value='Submit Application']"));
 
@@ -31,25 +33,40 @@ public class ApplicationFormTest {
 			testDriver.findElement(By.xpath("//input[@name='gender'][@value='" + gender.toLowerCase() + "']")).click();
 			testDriver.findElement(By.name("email")).sendKeys(email);
 			testDriver.findElement(By.name("phonenumber")).sendKeys(phoneNumber);
-			
+
 			Select courseSelect = new Select(testDriver.findElement(By.name("course")));
 			courseSelect.selectByValue("sdet");
 			Select sourceSelect = new Select(testDriver.findElement(By.name("source")));
 			sourceSelect.selectByValue("website");
-						
+
 			testDriver.findElement(By.name("notarobot")).click();
+
+			TestAsst.sleep(6);
 			if (sumbitButton.isEnabled())
 				sumbitButton.click();
-			
-			String realMessage = testDriver.findElement(By.cssSelector(".alert.alert-success")).getText();
-			
+
+			JavascriptExecutor js = (JavascriptExecutor) testDriver;
+			By locator = By.cssSelector(".alert.alert-success");
+			String realMessage = "";
+			if (TestAsst.containsElement(testDriver, locator)) {
+				WebElement goodTextElment = testDriver.findElement(locator);
+				js.executeScript("window.scrollTo(0, 0)");
+				realMessage = goodTextElment.getText();
+			} else {
+				js.executeScript("window.scrollTo(0, 0)");
+				TestAsst.sleep(3);
+				throw new Exception("The application has been denied.");
+			}
+
 			TestAsst.sleep(3);
 			System.out.println("Test passed.");
 			System.out.println(realMessage);
 		} catch (Exception e) {
-			System.out.println("Bad shit happened!");
-			e.printStackTrace();
+			System.out.println("Test failed!");
+			System.out.println(e.getMessage());
 		} finally {
+			System.out.println("Test completed.");
+			testDriver.close();
 			testDriver.quit();
 		}
 
