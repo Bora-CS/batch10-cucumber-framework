f320cf36f918d7166e7957be384e436d57e87252
Hui Yang <hui.yang.3737@gmail.com>
2023-08-06 11:32:48 -0400

    updated StudentRegistrationFormTest

--- src/test/java/hui_automation/UtilsTester.java
+++ src/test/java/hui_automation/UtilsTester.java
@@ -1,9 +1,14 @@
 package hui_automation;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class UtilsTester {
 
 	public static void main(String[] args) {
-		System.out.println(Testkeys.getUniqueMillsTimeStr().length());
+		LocalDate current = LocalDate.now();
+		String dayStr = current.format(DateTimeFormatter.ofPattern("MMMM"));
+		System.out.println(dayStr);
 	}
 
 }
