41253aca35c4df5de6b82273268821c9d77b622d
Hui Yang <hui.yang.3737@gmail.com>
2023-08-07 10:26:41 -0400

    updated AutomationPractice0807

--- src/test/java/hui_automation/AutomationPractice0807.java
+++ src/test/java/hui_automation/AutomationPractice0807.java
@@ -3,12 +3,12 @@ package hui_automation;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AutomationPractice0806 {
+public class AutomationPractice0807 {
 
 	public static void main(String[] args) {
 		WebDriver autoDriver = new ChromeDriver();
@@ -17,15 +17,15 @@ public class AutomationPractice0806 {
 			autoDriver.manage().window().maximize();
 			String targetRetailUrl = "https://www.target.com/";
 			autoDriver.get(targetRetailUrl);
-			WebElement searchBox = autoDriver.findElement(By.xpath("//input[@type='search']"));
-			searchBox.sendKeys("men's shoes"+ Keys.ENTER);
-			String currentUrl = autoDriver.getCurrentUrl();
-			
-			System.out.println(currentUrl);
+//			WebElement searchBox = autoDriver.findElement(By.xpath("//input[@type='search']"));
+//			searchBox.sendKeys("men's shoes"+ Keys.ENTER);
+			WebElement categories = autoDriver.findElement(By.xpath("//a[@aria-label='Categories']"));
+			categories.click();
+
 			Testkeys.pause(3);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			autoDriver.quit();
 			System.out.println("Automation completed.");
 		}
