579c26ea5f193cc6e543edad2af224aa9b6acfc4
Helen Ahn <helenhjahn@gmail.com>
2023-08-07 09:43:16 -0400

    class update

---
+++ src/test/java/helen/DataDriven_test2.java
@@ -0,0 +1,17 @@
+package helen;
+
+import org.openqa.selenium.WebDriver;
+
+public class DataDriven_test2 {
+
+	
+	
+	
+	public static void testForm() {
+		WebDriver localDriver = DataDriven_ApplicationForm.driverFactory();
+	}
+	
+	
+	
+	
+}
