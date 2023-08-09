82821857fbe8898ea0892d44034e4ec5f51d906b
Helen Ahn <helenhjahn@gmail.com>
2023-08-08 20:14:45 -0400

    added new classes

---
+++ src/test/java/pojo_helen/Education.java
@@ -0,0 +1,21 @@
+package pojo_helen;
+
+public class Education {
+
+
+	String username = username;
+	String password = password;
+	String school = school;
+	String degree = degree;
+	String fieldOfStudy = fieldOfStudy;
+	String from = fieldOfStudy;
+	String to = to;
+	boolean current = false;
+	String description = description;
+	String expectedErrorMessage = expectedErrorMessage;
+	
+	
+	
+	
+	
+}
