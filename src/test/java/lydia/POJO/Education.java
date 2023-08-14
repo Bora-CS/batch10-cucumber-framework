d6984dbb9cb1727b294b64a64acd0e65e932e17c
Lydia Liu <w.lydia.liu@gmail.com>
2023-08-14 12:03:25 -0400

    updated

--- src/test/java/lydia/POJO/Education.java
+++ src/test/java/lydia/POJO/Education.java
@@ -1,26 +1,37 @@
 package lydia.POJO;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Education {
 
-		public String school;
-		public String degree;
-		public String fieldOfStudy;
-		public String from;
-		public String to;
-		public boolean current;
-		public String description;
+	public String school;
+	public String degree;
+	public String fieldOfStudy;
+	public String from;
+	public String to;
+	public boolean current;
+	public String description;
+	public List<String> expectedErrors;
+	public boolean isHappyPath;
 
-		//constructor:
-		public Education(String school, String degree, String fieldOfStudy, String from, boolean current,String to,
-				String description) {
-			this.school = school;
-			this.degree = degree;
-			this.fieldOfStudy = fieldOfStudy;
-			this.from = from;
-			this.current = current;
-			this.to = to;
-			this.description = description;
+	public Education(String school, String degree, String fieldOfStudy, String from, String to, boolean current,
+			String description, String[] expectedErrors) {
+		this.school = school;
+		this.degree = degree;
+		this.fieldOfStudy = fieldOfStudy;
+		this.from = from;
+		this.to = to;
+		this.current = current;
+		this.description = description;
+		if (expectedErrors == null) {
+			this.expectedErrors = new ArrayList<>();
+		} else {
+			this.expectedErrors = Arrays.asList(expectedErrors);
 		}
-
+		this.isHappyPath = this.expectedErrors.size() == 0;
 	}
+}
+
 
