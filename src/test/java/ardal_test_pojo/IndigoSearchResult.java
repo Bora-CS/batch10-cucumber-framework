ae4208926bbe262e29d7ac8e2fd20da73e7125eb
Ardal Akbar <ardal002713@gmail.com>
2023-08-17 12:34:21 -0700

    Bunch of practices

---
+++ src/test/java/ardal_test_pojo/IndigoSearchResult.java
@@ -0,0 +1,17 @@
+package ardal_test_pojo;
+
+public class IndigoSearchResult {
+
+	public int id;
+	public double price;
+	public String title;
+
+	public IndigoSearchResult(int id, double price, String title) {
+		this.id = id;
+		this.price = price;
+		this.title = title;
+	}
+
+	
+	
+}
