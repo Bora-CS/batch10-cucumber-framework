ca246018b30c907130fd0fc201aa6b58dbd7d3ec
Lena Chen <chenjiayu632@gmail.com>
2023-08-18 07:30:51 -0400

    some changes

---
+++ src/test/java/lenaTest/Api/Api_Logging.java
@@ -0,0 +1,55 @@
+package lenaTest.Api;
+
+import java.util.HashMap;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Api_Logging {
+
+	public static void main(String[] args) {
+		String endpoint = "/api/auth";
+		RestAssured.baseURI = "https://boratech-practice-app.onrender.com";
+		RequestSpecification request = RestAssured.given();
+		
+		HashMap<String,String> body = new HashMap<>();
+		body.put("email", "chenlena@outlook.com");
+		body.put("password", "798911");
+		
+		request.body(body);
+		request.header("Content-Type", "application/json");
+		Response response = request.post(endpoint);
+		System.out.println(response.getStatusCode());
+
+	}
+}
+//package apis;
+//
+//import java.util.HashMap;
+//
+//import io.restassured.RestAssured;
+//import io.restassured.response.Response;
+//import io.restassured.specification.RequestSpecification;
+//
+//public class Login {
+//
+//	public static void main(String[] args) {
+//
+//		String endpoint = "/api/auth";
+//		RestAssured.baseURI = "https://boratech-practice-app.onrender.com";
+//		RequestSpecification request = RestAssured.given();
+//
+//		HashMap<String, String> body = new HashMap<>();
+//		body.put("email", "muradil.erkin@boratechschool.com");
+//		body.put("password", "Boratech");
+//
+//		request.body(body);
+//		request.header("Content-Type", "application/json");
+//
+//		Response response = request.post(endpoint);
+//		System.out.println(response.body().asPrettyString());
+//
+//	}
+//
+//}
