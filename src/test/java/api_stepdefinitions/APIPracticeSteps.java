8320d7dff01460562e38799580a848a713df741c
Muradil Erkin <muradil.erkin@boratechschool.com>
2023-08-29 21:50:29 -0400

    8-29 API with Cucumber

---
+++ src/test/java/api_stepdefinitions/APIPracticeSteps.java
@@ -0,0 +1,73 @@
+package api_stepdefinitions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class APIPracticeSteps {
+	Response resp;
+
+	@Given("call login api with {string} and {string}")
+	public void call_login_api(String email, String password) {
+
+		RestAssured.baseURI = "https://boratech-practice-app.onrender.com/";
+		RequestSpecification request = RestAssured.given();
+
+		request.header("Content-Type", "application/json");
+
+		HashMap<String, String> body = new HashMap<>();
+		body.put("email", email);
+		body.put("password", password);
+		request.body(body);
+
+		resp = request.post("api/auth");
+		System.out.println(resp.getBody().asPrettyString());
+
+	}
+
+	@Then("we should get reponse code {int}")
+	public void we_should_get_reponse_code(int expectedStatusCode) {
+		
+		int actualStatusCode = resp.getStatusCode();
+		
+		Assertions.assertTrue(expectedStatusCode == actualStatusCode, "The actual status code is not match!");
+		
+	}
+	
+	
+	
+	@Given("login with user and validate status")
+	public void login_with_user_and_validate_status(DataTable dataTable) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+		
+		List<Map<String, String>>	datas = dataTable.asMaps();
+		
+		System.out.println( "data from featuer file: "+ datas.get(0) );
+		System.out.println( "data from featuer file: "+ datas.get(1) );
+
+	   
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/test/java/runners/RunAPI.java
@@ -0,0 +1,20 @@
+package runners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/APITest.feature")
+
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value="apis, api_stepdefinitions")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@api")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty ,html:target/apiReport/index.html")
+@ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME, value ="false")
+public class RunAPI {
+
+}
