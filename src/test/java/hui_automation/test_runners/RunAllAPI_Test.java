023d5a3c1153adf467649674552daf703ae089bb
Hui Yang <hui.yang.3737@gmail.com>
2023-09-01 14:56:27 -0400

    added RunAllTest added RunAllAPI_Test

---
+++ src/test/java/hui_automation/test_runners/RunAllAPI_Test.java
@@ -0,0 +1,12 @@
+package hui_automation.test_runners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+public class RunAllAPI_Test {
+
+}
---
+++ src/test/java/hui_automation/test_runners/RunAllTest.java
@@ -0,0 +1,15 @@
+package hui_automation.test_runners;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "hui_automation.steps")
+public class RunAllTest {}
