4095810d50ab6e928a846534b806ee6cbee5195b
Hui Yang <hui.yang.3737@gmail.com>
2023-09-02 17:21:57 -0400

    updated Hooks

--- src/test/java/hui_automation/steps/api/Hooks.java
+++ src/test/java/hui_automation/steps/api/Hooks.java
@@ -1,9 +1,15 @@
 package hui_automation.steps.api;
 
-import hui_automation.utilities.DataManager;
+import io.cucumber.java.Before;
 import io.cucumber.java.After;
+import hui_automation.utilities.DataManager;
 
 public class Hooks {
+
+	@Before("@api")
+	public void setup() {
+		DataManager.getInstance();
+	}
 
 	@After("@api")
 	public void cleanup() {
