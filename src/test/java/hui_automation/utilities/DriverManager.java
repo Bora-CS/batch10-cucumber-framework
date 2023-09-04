d31595fc980557e03dc1ff0d89fbf13a4847dd25
Hui Yang <hui.yang.3737@gmail.com>
2023-09-04 12:08:05 -0400

    updated DriverManager

--- src/test/java/hui_automation/utilities/DriverManager.java
+++ src/test/java/hui_automation/utilities/DriverManager.java
@@ -4,16 +4,20 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
 	private static WebDriver driver = null;
 
-	private DriverManager() {};
+	private DriverManager() {
+	};
 
 	public static WebDriver getInstance() {
 		if (driver == null) {
-			driver = new ChromeDriver();
+			ChromeOptions co = new ChromeOptions();
+			co.addArguments("--remote-allow-origins=*");
+			driver = new ChromeDriver(co);
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		}
