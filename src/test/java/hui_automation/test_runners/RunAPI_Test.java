6f0fd3b1580a94bac7e760ae189e558e32a3653a
Hui Yang <hui.yang.3737@gmail.com>
2023-09-09 23:54:19 -0400

    updated test_runners

--- src/test/java/hui_automation/test_runners/RunAPI_Test.java
+++ src/test/java/hui_automation/test_runners/RunAPI_Test.java
@@ -1,9 +1,9 @@
 package hui_automation.test_runners;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.ConfigurationParameter;
 
 import static io.cucumber.junit.platform.engine.Constants.*;
 
--- src/test/java/hui_automation/test_runners/RunParallelTest.java
+++ src/test/java/hui_automation/test_runners/RunParallelTest.java
@@ -1,11 +1,11 @@
 package hui_automation.test_runners;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
-
-import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.ConfigurationParameter;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
